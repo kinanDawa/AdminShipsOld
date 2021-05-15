b357e5f19a4839edfc2c68c54020c95899b06950
kinan <dawakinan@gmail.com>
2021-05-15 13:13:04 +0300

    Creating HTML pages
    muliple HTML pages for Front End

--- src/main/java/com/kindsonthegenius/fleetapp/controllers/ClientController.java
+++ src/main/java/com/kindsonthegenius/fleetapp/controllers/ClientController.java
@@ -1,8 +1,12 @@
 package com.kindsonthegenius.fleetapp.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class ClientController {
-
+	@GetMapping("/clients")
+	public String getClients() {
+		return "client";
+}
 }
