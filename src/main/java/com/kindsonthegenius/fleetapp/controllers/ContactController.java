67631b7cf33181528970c7f6f072fee6cc44f885
kinan <dawakinan@gmail.com>
2021-05-15 20:43:11 +0300

    Contact Controller

--- src/main/java/com/kindsonthegenius/fleetapp/controllers/ContactController.java
+++ src/main/java/com/kindsonthegenius/fleetapp/controllers/ContactController.java
@@ -1,8 +1,13 @@
 package com.kindsonthegenius.fleetapp.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class ContactController {
+	@GetMapping("/contacts")
+	public String getClients() {
+		return "contact";
 
+}
 }
