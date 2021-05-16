9d6bce18fee7b13a5495ba5ee1a123e030a3840f
kinan <dawakinan@gmail.com>
2021-05-16 15:39:56 +0300

    adding statements to the controllers

--- src/main/java/com/kindsonthegenius/fleetapp/controllers/VehicleModelController.java
+++ src/main/java/com/kindsonthegenius/fleetapp/controllers/VehicleModelController.java
@@ -1,8 +1,14 @@
 package com.kindsonthegenius.fleetapp.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class VehicleModelController {
+	
+	@GetMapping("/vehiclemodel")
+	public String getvehicleModel() {
+		return "VehicleModel";
+	}
 
 }
--- src/main/java/com/kindsonthegenius/fleetapp/controllers/VehicleStatusController.java
+++ src/main/java/com/kindsonthegenius/fleetapp/controllers/VehicleStatusController.java
@@ -1,8 +1,14 @@
 package com.kindsonthegenius.fleetapp.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class VehicleStatusController {
+	
+	@GetMapping("/vehiclestatus")
+	public String getvehicleStatus() {
+		return "VehicleStatus";
+	}
 
 }
