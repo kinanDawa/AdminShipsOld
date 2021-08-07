51bf4548068de9a4e273db70b3bcf9d858a5cbbe
kinan <dawakinan@gmail.com>
2021-08-08 00:52:27 +0300

    error fixing

--- src/main/java/com/kindsonthegenius/fleetapp/controllers/CountryController.java
+++ src/main/java/com/kindsonthegenius/fleetapp/controllers/CountryController.java
@@ -6,7 +6,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
-import com.kindsonthegenius.fleetapp.models.Country;
+import com.kindsonthegenius.fleetapp.models.*;
 import com.kindsonthegenius.fleetapp.services.CountryService;
 
 
--- src/main/java/com/kindsonthegenius/fleetapp/services/CountryService.java
+++ src/main/java/com/kindsonthegenius/fleetapp/services/CountryService.java
@@ -1,18 +1,18 @@
 package com.kindsonthegenius.fleetapp.services;
 
-import java.util.List;
+import java.util.*;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.kindsonthegenius.fleetapp.models.Country;
-import com.kindsonthegenius.fleetapp.repositories.CountryRepository;
+import com.kindsonthegenius.fleetapp.repositories.*;
 
 @Service
 public class CountryService {
 	
-	@Autowired
+	 @Autowired
 	private CountryRepository countryRepository;
 	//the service is getting data from Repository
 	
@@ -21,19 +21,18 @@ public class CountryService {
 		return countryRepository.findAll();
 	}	
 	
-	//Get Country By Id
-	public Optional<Country> findById(int id) {
-		return countryRepository.findById(id);
-	}	
 	
-	//Delete Country
-	public void delete(int id) {
-		countryRepository.deleteById(id);
-	}
-	
-	//Update Country
-	public void save( Country country) {
-		countryRepository.save(country);
-	}
+	/*
+	 * //Get Country By Id public Optional<Country> findById(int id) { return
+	 * countryRepository.findById(id); }
+	 * 
+	 * 
+	 * //Delete Country public void delete(int id) {
+	 * countryRepository.deleteById(id); }
+	 * 
+	 * //Update Country public void save( Country country) {
+	 * countryRepository.save(country); }
+	 */
+	 
 
 }
